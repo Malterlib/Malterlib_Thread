6d76052556c779c39afd71c8857bcab1b9e8901c
Erik Olofsson <erik@olofsson.info>
2026-04-05 17:19:26 +0200

    Update licensing to Apache 2.0 with LLVM Exceptions

--- Source/Malterlib_Thread_ThreadLocal.cpp
+++ Source/Malterlib_Thread_ThreadLocal.cpp
@@ -1,5 +1,5 @@
-// Copyright © 2015 Hansoft AB
-// Distributed under the MIT license, see license text in LICENSE.Malterlib
+// Copyright © Unbroken AB
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <Mib/Core/Core>
 #include "../../Core/Source/Malterlib_Core_System.h"
